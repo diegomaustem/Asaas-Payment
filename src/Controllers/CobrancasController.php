b985daceaac5f90a711065793ddd06867339a338
diegomaustem <diego.maustem@gmail.com>
2025-06-04 09:58:35 -0300

    :sparkles: feat: Implementação criação de cobranças

--- src/Controllers/CobrancasController.php
+++ src/Controllers/CobrancasController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 use App\Services\Asaas\Asaas;
+use DateTime;
 use Swoole\Coroutine\Channel;
 class CobrancasController
 {
@@ -32,24 +33,65 @@ class CobrancasController
 
     public function store(Request $request, Response $response) 
     {
-        // $dadosEntrada = json_decode($request->rawContent(), true);
-        // $validacao = $this->validaEntradas($dadosEntrada);
+        $dadosCobranca = json_decode($request->rawContent(), true);
+        $validacao = $this->validaCobranca($dadosCobranca);
+
+        if($validacao !== true) {
+            return ['status' => 422, 'error' => $validacao];
+        } else {
+            $channelCobranca = new Channel(1);
+
+            go(function () use ($channelCobranca, $dadosCobranca) {
+                $metodo = 'POST';
+                $endPoint = '/v3/payments';
+
+                $asaas = new Asaas($metodo, $endPoint, json_encode($dadosCobranca));
+                $resposta = $asaas->requisicaoAPIAsaas();
+
+                $channelCobranca->push($resposta);
+            });
+
+            $resultado = $channelCobranca->pop();
+
+            if ($resultado['status'] == 200) {
+                return ['status' => $resultado['status'], 'message' => 'Cobrança criada com sucesso.', 'data' => json_decode($resultado['body'])];
+            } else {
+                return ['status' => $resultado['status'], 'error' => $resultado['error']];
+            }
+        }
     }
     
-    private function validaEntradas(array $dadosEntrada): array|bool
+    private function validaCobranca(array $dadosCobranca): array|bool
     {
         $errors = [];
 
-        if (!isset($dadosEntrada['nome']) || !is_string($dadosEntrada['nome']) || trim($dadosEntrada['nome']) === '') {
-            $errors['nome'] = 'O campo "nome" é obrigatório e deve ser uma string não vazia.';
-        } elseif (strlen($dadosEntrada['nome']) < 3) {
-            $errors['nome'] = 'O nome deve ter pelo menos 3 caracteres.';
+        if (!isset($dadosCobranca['customer']) || !is_string($dadosCobranca['customer']) || trim($dadosCobranca['customer']) === '') {
+            $errors['customer'] = 'O campo "customer" é obrigatório e deve ser uma string não vazia.';
+        } elseif (strlen($dadosCobranca['customer']) < 3) {
+            $errors['customer'] = 'O customer deve ter pelo menos 3 caracteres.';
         }
 
-        if (!isset($dadosEntrada['idade']) || !is_int($dadosEntrada['idade'])) {
-            $errors['idade'] = 'O campo "idade" é obrigatório e deve ser um número inteiro.';
-        } elseif ($dadosEntrada['idade'] <= 0 || $dadosEntrada['idade'] > 150) {
-            $errors['idade'] = 'A idade deve ser um número entre 1 e 150.';
+        if (!isset($dadosCobranca['billingType']) || !is_string($dadosCobranca['billingType']) || trim($dadosCobranca['billingType']) === '') {
+            $errors['billingType'] = 'O campo "billingType" é obrigatório e deve ser uma string não vazia.';
+        } elseif (strtoupper($dadosCobranca['billingType']) !== 'PIX') {
+            $errors['billingType'] = 'O billingType deve ser "PIX".';
+        }
+
+        if (!isset($dadosCobranca['value']) || !is_numeric($dadosCobranca['value'])) {
+            $errors['value'] = 'O campo "value" é obrigatório e deve ser um número.';
+        } elseif ($dadosCobranca['value'] <= 0) {
+            $errors['value'] = 'O valor deve ser maior que zero.';
+        }
+
+        if (!isset($dadosCobranca['dueDate']) || !is_string($dadosCobranca['dueDate']) || trim($dadosCobranca['dueDate']) === '') {
+            $errors['dueDate'] = 'O campo "dueDate" é obrigatório e deve ser uma string não vazia.';
+        } else {
+            $date = DateTime::createFromFormat('Y-m-d', $dadosCobranca['dueDate']);
+            if (!$date || $date->format('Y-m-d') !== $dadosCobranca['dueDate']) {
+                $errors['dueDate'] = 'O campo "dueDate" deve estar no formato YYYY-MM-DD.';
+            } elseif ($date < new DateTime('today')) {
+                $errors['dueDate'] = 'A data de vencimento não pode ser anterior ao dia atual.';
+            }
         }
 
         return !empty($errors) ? [
